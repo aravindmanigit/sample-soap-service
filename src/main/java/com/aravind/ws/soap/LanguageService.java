6c176fab6f235a2d6734697729394659cd99505f
aravindmanigit <aravind.mani.git@outlook.com>
2018-03-17 21:54:45 +1100

    added some annotations in class and method level

--- src/main/java/com/aravind/ws/soap/LanguageService.java
+++ src/main/java/com/aravind/ws/soap/LanguageService.java
@@ -3,21 +3,34 @@ package com.aravind.ws.soap;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import com.aravind.ws.model.Language;
 import com.aravind.ws.serviceprovider.LanguageServiceProvider;
 import com.aravind.ws.serviceprovider.LanguageServiceProviderImpl;
 
-@WebService
+@WebService(serviceName="testService", name="AravindService", 
+			targetNamespace="http://soap.ws.aravind.com/LanguageService")
 public class LanguageService {
 	
 	private LanguageServiceProvider languageServiceProvider = new LanguageServiceProviderImpl();
 	
-	@WebMethod
-	public List<Language> getLanguageList()
+	@WebMethod(exclude=true)
+	public LanguageServiceProvider getLanguageServiceProvider() {
+		return languageServiceProvider;
+	}
+
+	@WebMethod(exclude=true)
+	public void setLanguageServiceProvider(LanguageServiceProvider languageServiceProvider) {
+		this.languageServiceProvider = languageServiceProvider;
+	}
+
+
+	@WebMethod(operationName="fetchLanguages", action="fetch_languages")
+	public List<String> getLanguageList()
 	{
-		return languageServiceProvider.getLanguages();
+		return null;
 	}
 
 }
